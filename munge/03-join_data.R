7d02f28cd4987dba0d6135287441e2db301d8390
eamoncaddigan <eamon.caddigan@gmail.com>
2016-11-03 14:46:38 -0400

    Combining party and congressional data to presidents

---
+++ munge/03-join_data.R
@@ -0,0 +1,16 @@
+# Join all of the data in prep for regression
+
+# Add the presidents' party affiliation
+mandates <- presidents %>%
+  mutate(party = as.factor(party)) %>%
+  left_join(mandates, ., by = "president")
+
+# Add the share of the senate and house held by the newly-elected president's
+# party. Look at both incoming and outgoing shares.
+mandates <- congress %>% 
+  rename(incoming = voted_in, outgoing = voted_out) %>% 
+  gather("term", "election_year", incoming, outgoing) %>%
+  unite(chamber, term, chamber, sep = "_") %>%
+  mutate(chamber = paste0(chamber, "_share")) %>%
+  spread(chamber, share) %>%
+  left_join(mandates, ., by = c("party", "election_year"))
